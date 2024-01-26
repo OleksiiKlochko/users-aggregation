799601a7c56660b5195bea2193bf8082fb0ced27
Oleksii Klochko <klochkoalexey9@gmail.com>
2024-01-26 19:22:50 +0200

    Add validation for configuration properties

--- src/main/java/users/aggregation/configuration/DataSourcesProperties.java
+++ src/main/java/users/aggregation/configuration/DataSourcesProperties.java
@@ -1,27 +1,32 @@
 package users.aggregation.configuration;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @ConfigurationProperties
-public record DataSourcesProperties(List<DataSource> dataSources) {
+@Validated
+public record DataSourcesProperties(List<@NotNull @Valid DataSource> dataSources) {
 
     public record DataSource(
-            String name,
-            String strategy,
-            String url,
-            String table,
-            String user,
-            String password,
-            Mapping mapping
+            @NotEmpty String name,
+            @NotEmpty String strategy,
+            @NotEmpty String url,
+            @NotEmpty String table,
+            @NotEmpty String user,
+            @NotEmpty String password,
+            @NotNull @Valid Mapping mapping
     ) {
 
         public record Mapping(
-                String id,
-                String username,
-                String name,
-                String surname
+                @NotEmpty String id,
+                @NotEmpty String username,
+                @NotEmpty String name,
+                @NotEmpty String surname
         ) {
         }
     }
